d6995bbf64da5d846a2dfb9e1e7117e9d116d6d4
Liza Daly <lizadaly@gmail.com>
2016-10-14 15:30:53 -0400

    2to3 output

--- fabtools/require/files.py
+++ fabtools/require/files.py
@@ -190,7 +190,7 @@ def file(path=None, contents=None, source=None, url=None, md5=None,
 
     # Ensure correct mode
     if use_sudo and mode is None:
-        mode = oct(0666 & ~int(umask(use_sudo=True), base=8))
+        mode = oct(0o666 & ~int(umask(use_sudo=True), base=8))
     if mode and _mode(path, use_sudo) != mode:
         func('chmod %(mode)s "%(path)s"' % locals())
 
